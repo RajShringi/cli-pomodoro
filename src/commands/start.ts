722283abf7e58b6c957be4b52f5f0a092cd6ab1b
Raj Shringi <raj24shringi@gmail.com>
2026-07-24 20:35:46 +0530

    implement start command

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,18 +1,23 @@
+import { getConfig } from "../storage/config.js";
 import { runCountdown } from "../timer/countdown.js";
 import { formatTime } from "../utils/time.js";
 
-export async function start(_args: string[]) {
-  let totalSeconds = 10;
-  console.log(`🍅 start working`);
+export function start(_args: string[]) {
+  const { workDuration } = getConfig();
+  const totalSeconds = workDuration * 60;
+  console.log(`🍅 Work session (${workDuration} minutes)\n`);
 
   runCountdown(totalSeconds, {
-    onTick(seconds) {
-      process.stdout.write(`\r${formatTime(seconds)}`);
-    },
-
-    onComplete() {
-      process.stdout.write("\n");
-      console.log("🍅 Session complete!");
-    },
+    onTick,
+    onComplete,
   });
+}
+
+function onTick(seconds: number) {
+  process.stdout.write(`\r${formatTime(seconds)} remaining`);
+}
+
+function onComplete() {
+  process.stdout.write("\n");
+  console.log("🍅 Session complete!");
 }
